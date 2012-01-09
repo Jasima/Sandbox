bfba847d58ef94555f519ef6b9f77005477cc06d
Jasima <jesi.azu@gmail.com>
2012-01-09 17:05:14 +0530

    Init FizzBuzz

---
+++ FizzBuzz/FizzBuzz.Core/FzBz.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz.Core
+{
+    public class FzBz
+    {
+        public string GetFizzBuzz(int input)
+        {
+            if (input % 15 == 0) return "FizzBuzz";
+            else if ((input % 3) == 0) return "Fizz";
+            else if ((input % 5) == 0) return "Buzz";
+            return string.Empty;
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FizzBuzz.Core;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            bool keepGoing = true;
+            FzBz fb = new FzBz(); //Constructor
+            while (keepGoing)
+            {
+                Console.WriteLine("Enter a number or X to exit.");
+                string input = Console.ReadLine();
+                if (input.Trim().ToLower() == "x") { keepGoing = false; continue; }
+                int inputAsNumber = 0;
+                int.TryParse(input, out inputAsNumber);
+
+                if (inputAsNumber == 0 || inputAsNumber >= 100 || inputAsNumber < 1)
+                    Console.WriteLine("Invalid number. Please enter a number between 1 and 100");
+                else
+                {
+                    for (i = 1; i <= inputAsNumber; i++)
+                    {
+                        var result = fb.GetFizzBuzz(i);
+                        if (string.IsNullOrEmpty(result)) continue;
+                        Console.WriteLine(string.Format("{0} - {1}", i, result));
+                    }
+                }
+            }
+        }
+    }
+}
